9b9a413386d8ced63b58b4117aceee8878be0f14
shantel23 <78841461+shantel23@users.noreply.github.com>
2022-12-19 14:36:31 +0300

    Add files via upload

---
+++ Makefile.c
@@ -0,0 +1,15 @@
+MyProject : countdown_rafael2k.o countdown_rafael2k.c   
+    gcc countdown_rafael2k.o -o countdown_rafael2k
+    ./a.exe
+
+MyProject : countdown_rafael2k.c
+    gcc -c countdown_rafael2k.c -o countdown_rafael2k.o
+
+installemsdk:
+    cd .. && git clone https://github.com/emscripten-core/emsdk.git
+    cd ../emsdk && emsdk install latest && emsdk activate latest &&emsdk_env.bat && emcc countdown_rafael2k.c -s WASM=1 -o pge.html
+    npm i http-server -g
+
+runserver:
+    cd ../emrun pge.html
+
